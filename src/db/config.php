3490fd08dd60417fcf114174ecf5678edee9bb3a
danplatypus30 <51561274+danplatypus30@users.noreply.github.com>
2020-10-16 01:30:43 +0800

    changes

--- src/db/config.php
+++ src/db/config.php
@@ -1,9 +1,9 @@
 <?php
-   define('DB_SERVER', 'localhost');
+   define('DB_SERVER', '127.0.0.1');
    // The DB credentials should not be be stored in source control, but they are include here to provide a complete example.  
-   define('DB_USERNAME', 'sec_user');
-   define('DB_PASSWORD', 'DgWWTcq!SfjP49Xr');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', 'root');
    define('DB_DATABASE', 'security_challenge');
-   $db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+   $db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE, 3306);
    $db->set_charset("utf8");
 ?>
